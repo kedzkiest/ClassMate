70a0668f416e942a459a9a6755dcc2576c8d66c7
kedzkiest <kaede12kazu22@gmail.com>
2023-01-19 08:54:52 +0900

    uploaded entire project from EN version

---
+++ NovelGame_EN/Assets/C#/CharacterAnimation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimation : MonoBehaviour
+{
+    private Animator anim;
+    private static readonly int Default = Animator.StringToHash("Default");
+    private static readonly int FindSomething = Animator.StringToHash("FindSomething");
+    private static readonly int Walking = Animator.StringToHash("Walking");
+
+    void Start()
+    {
+        anim = gameObject.GetComponent<Animator>();
+    }
+
+    public void Action_FindSomething()
+    {
+        anim.SetTrigger(FindSomething);
+    }
+
+    public void Action_Walking()
+    {
+        anim.SetTrigger(Walking);
+    }
+
+    public void Action_Neutral(){
+        anim.ResetTrigger(FindSomething);
+        anim.ResetTrigger(Walking);
+        anim.SetTrigger(Default);
+    }
+
+}
